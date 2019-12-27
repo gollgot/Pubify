597cbcded1b146625a6899cd34f7cf6bedbd1878
Doran Kayoumi <dorankayoumi@gmail.com>
2019-12-27 22:29:25 +0100

    Update comments

--- DB/Triggers/staff.sql
+++ DB/Triggers/staff.sql
@@ -10,7 +10,7 @@ BEGIN
 
     SET current_nb_managers = (SELECT COUNT(*) FROM Manager);
 
-    -- If there is only one manager, then we can't delete him
+    -- If there is only one manager in the DB, then he or she can't be deleted
     IF current_nb_managers = 1 THEN
         -- return an `unhandeled used-defined exception`
         -- see : https://dev.mysql.com/doc/refman/5.5/en/signal.html
