d7a9f3e057c38bacd5b18efd03436cbca8a83563
Doran Kayoumi <dorankayoumi@gmail.com>
2019-12-27 14:36:16 +0100

    Add trigger for manager delete

---
+++ DB/Triggers/staff.sql
@@ -0,0 +1,23 @@
+USE PUBify;
+
+DELIMITER $$
+
+CREATE TRIGGER before_manager_delete
+BEFORE DELETE ON Manager
+FOR EACH ROW
+BEGIN
+    DECLARE current_nb_managers INTEGER;
+
+    SET current_nb_managers = (SELECT COUNT(*) FROM Manager);
+
+    -- If there is only one manager, then we can't delete him
+    IF current_nb_managers = 1 THEN
+        -- return an `unhandeled used-defined exception`
+        -- see : https://dev.mysql.com/doc/refman/5.5/en/signal.html
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'There needs to be more than one manager to delete one!';
+    END IF;
+END
+$$
+
+DELIMITER ;
