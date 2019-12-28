4d103a48634d500915d6b122a04d338d13d7cd72
Doran Kayoumi <dorankayoumi@gmail.com>
2019-12-28 19:46:16 +0100

    Add before updates and move common code in procedure

--- DB/procedures.sql
+++ DB/procedures.sql
@@ -2,7 +2,18 @@ USE PUBify;
 
 DELIMITER $$
 
+-- ------------------------ --
+-- GENERAL USAGE PROCEDURES --
+-- ------------------------ --
+
 CREATE FUNCTION is_negative_decimal(num DECIMAL)
+RETURNS BOOLEAN
+DETERMINISTIC
+BEGIN
+    RETURN IF(num < 0, TRUE, FALSE);
+END $$
+
+CREATE FUNCTION is_negative_int(num INT)
 RETURNS BOOLEAN
 DETERMINISTIC
 BEGIN
@@ -14,6 +25,44 @@ RETURNS BOOLEAN
 DETERMINISTIC
 BEGIN
     RETURN IF(`time` < 0, TRUE, FALSE);
+END $$
+
+-- ------------------- --
+-- SPECIFIC PROCEDURES --
+-- ------------------- --
+
+CREATE PROCEDURE validate_alcohol_level(alcohol_level DECIMAL)
+BEGIN
+    IF is_negative_decimal(alcohol_level) THEN
+        -- return an `unhandeled used-defined exception`
+        -- see : https://dev.mysql.com/doc/refman/5.5/en/signal.html
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Alcohol level can\'t be negative!';
+    END IF;
+END $$
+
+CREATE PROCEDURE validate_happy_hour_reduction(reduction INT)
+BEGIN
+    DECLARE max_reduction_percent INT;
+    SET max_reduction_percent = 100;
+
+    -- TODO maybe create a function `within_range` ?
+    IF is_negative_int(reduction) AND reduction <= max_reduction_percent THEN
+        -- return an `unhandeled used-defined exception`
+        -- see : https://dev.mysql.com/doc/refman/5.5/en/signal.html
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Alcohol level can\'t be negative!';
+    END IF;
+END $$
+
+CREATE PROCEDURE validate_happy_hour_duration(duration TIME)
+BEGIN
+    IF is_negative_time(duration) THEN
+        -- return an `unhandeled used-defined exception`
+        -- see : https://dev.mysql.com/doc/refman/5.5/en/signal.html
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'The duration of a Happy Hour can\'t be negative!';
+    END IF;
 END $$
 
 DELIMITER ;
--- DB/triggers.sql
+++ DB/triggers.sql
@@ -6,27 +6,30 @@ CREATE TRIGGER before_drink_insert
 BEFORE INSERT ON Drink
 FOR EACH ROW
 BEGIN
-    IF is_negative_decimal(NEW.alcoholLevel) THEN
-        -- return an `unhandeled used-defined exception`
-        -- see : https://dev.mysql.com/doc/refman/5.5/en/signal.html
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Alcohol level can\'t be negative!';
-    END IF;
+    CALL validate_alcohol_level(NEW.alcoholLevel);
+END $$
+
+CREATE TRIGGER before_drink_update
+BEFORE UPDATE ON Drink
+FOR EACH ROW
+BEGIN
+    CALL validate_alcohol_level(NEW.alcoholLevel);
 END $$
 
 CREATE TRIGGER before_happy_hour_insert
 BEFORE INSERT ON HappyHour
 FOR EACH ROW
 BEGIN
-    DECLARE max_reduction_percent INT;
-    SET max_reduction_percent = 100;
+    CALL validate_happy_hour_duration(NEW.duration);
+    CALL validate_happy_hour_reduction(NEW.reductionPercent);
+END $$
 
-    IF is_negative_decimal(NEW.reductionPercent) AND NEW.reductionPercent <= max_reduction_percent THEN
-        -- return an `unhandeled used-defined exception`
-        -- see : https://dev.mysql.com/doc/refman/5.5/en/signal.html
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = '`reductionPercent` must be greater then 0 and lower than 100';
-    END IF;
+CREATE TRIGGER before_happy_hour_update
+BEFORE UPDATE ON HappyHour
+FOR EACH ROW
+BEGIN
+    CALL validate_happy_hour_duration(NEW.duration);
+    CALL validate_happy_hour_reduction(NEW.reductionPercent);
 END $$
 
 CREATE TRIGGER before_manager_delete
