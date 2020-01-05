32987e8c1a1464f398cc678639d6656817789d20
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2020-01-05 23:06:16 +0100

    Add linked drinks to happy hours index

--- src/app/Controllers/Admin/HappyHourController.php
+++ src/app/Controllers/Admin/HappyHourController.php
@@ -21,8 +21,19 @@ class HappyHourController extends Controller
         // Fetch the pdo connection from the container dependency
         $pdo = $this->container->db;
 
-        $query = $pdo->query("SELECT name, lastname, duration, reductionPercent, DATE_FORMAT(startAt, '%d.%m.%Y %H:%i') as startAt FROM HappyHour INNER JOIN Manager ON idManager = idStaff INNER JOIN Staff ON idStaff = id ORDER BY startAt");
+        // Fetch all happyhours
+        $query = $pdo->query("SELECT name, lastname, duration, reductionPercent, startAt, DATE_FORMAT(startAt, '%d.%m.%Y %H:%i') as startAtFormated FROM HappyHour INNER JOIN Manager ON idManager = idStaff INNER JOIN Staff ON idStaff = id ORDER BY startAt");
         $happyhours = $query->fetchAll();
+
+        // For each happy hour, fetch his linked drinks and add them to the happyHour array to pass them to the view
+        for($i = 0; $i < count($happyhours); ++$i){
+            $query = $pdo->prepare('SELECT vDrink.product_name FROM Drink_HappyHour INNER JOIN vDrink ON idDrink = vDrink.id  WHERE startAtHappyHour = :startAt');
+            $query->execute([
+                'startAt' => $happyhours[$i]['startAt']
+            ]);
+            $linkedDrinks = $query->fetchAll();
+            $happyhours[$i]['linkedDrinks'] = $linkedDrinks;
+        }
 
         return $this->render($response, 'Admin/HappyHours/index.html.twig', [
             'happyhours' => $happyhours
