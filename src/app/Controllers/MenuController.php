591145b7441a5c1d5134cd967a0db37d66cb6336
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2020-01-10 11:59:19 +0100

    Add menu page

--- src/app/Controllers/MenuController.php
+++ src/app/Controllers/MenuController.php
@@ -16,7 +16,22 @@ class MenuController extends Controller
      * @return mixed
      */
     public function showAction(Request $request, Response $response) {
-        return $this->render($response, 'Menu/show.html.twig', []);
+        $pdo = $this->container->db;
+
+        $query = $pdo->query("SELECT product_name, price FROM vFood WHERE endSaleDate >= NOW() OR endSaleDate IS NULL");
+        $foods = $query->fetchAll();
+
+        $query = $pdo->query("SELECT product_name, price, alcoholLevel, quantity FROM vDrink WHERE alcoholLevel > 0.0 AND (endSaleDate >= NOW() OR endSaleDate IS NULL) ORDER BY quantity DESC");
+        $drinksWithAlcohol = $query->fetchAll();
+
+        $query = $pdo->query("SELECT product_name, price, quantity FROM vDrink WHERE alcoholLevel = 0.0 AND (endSaleDate >= NOW() OR endSaleDate IS NULL) ORDER BY quantity DESC");
+        $drinksWithoutAlcohol = $query->fetchAll();
+
+        return $this->render($response, 'Menu/show.html.twig', [
+            'foods' => $foods,
+            'drinksWithAlcohol' => $drinksWithAlcohol,
+            'drinksWithoutAlcohol' => $drinksWithoutAlcohol,
+        ]);
     }
 
 }
